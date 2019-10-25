8cdf497726ba0aaf07e55555baaf3744919782ae
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-10-25 12:41:53 -0400

    added basic timer that moviesthrough questions.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
-    const question = [{ "question": "whats my name", "correct": 0, "answerChoices": ["dan", "bill", "dave", "steve"] },
-        { "question": "whats my sisters name", "correct": 1, "answerChoices": ["laura", "kelly", "mary", "pat"] }
-    ]
+    const question = [{
+                "question": "whats my name",
+                "correct": 0,
+                "answerChoices": ["dan", "bill", "dave", "steve"]
+            },
+            {
+                "question": "whats my sisters name",
+                "correct": 1,
+                "answerChoices": ["laura", "kelly", "mary", "pat"]
+            },
+
+            {
+                "question": "whats my brothers name?",
+                "correct": 2,
+                "answerChoices": ["bill", "steve", "brian", "bananaman"]
+            }
+        ]
+        //keeps track of what question we are on
+    let count = 0;
+    let qInterval;
+    displayQuestion();
 
 
-
-    displayQuestion()
 
 
 
     function displayQuestion() {
-        const questionDisplay = question[0];
+        $("#answers").empty();
+        const questionDisplay = question[count];
         $("#question").text(questionDisplay.question);
         for (let i = 0; i < questionDisplay.answerChoices.length; i++) {
             button = $("<button>");
@@ -24,6 +41,17 @@ $(document).ready(function() {
 
             isCorrect($(this).val(), questionDisplay.correct)
         })
+
+        nextQuestion()
+    }
+
+    function nextQuestion() {
+
+        qInterval = setTimeout(displayQuestion, 3000)
+        count++
+        if (count === question.length) {
+            displayScore()
+        }
     }
 
     function isCorrect(choice, answer) {
@@ -35,5 +63,9 @@ $(document).ready(function() {
         }
     }
 
+    function displayScore() {
+        console.log("you did it");
+        clearInterval(qInterval);
+    }
 
 })
